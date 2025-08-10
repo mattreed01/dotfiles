3d401b432660ded036336b01d1f32be1f0763256
mattreed01 <matthewreed0107@gmail.com>
2025-08-10 22:22:57 +0200

    Update telescope plugin to use fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,11 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make"
+    }
   },
   opts = {
     defaults = {
